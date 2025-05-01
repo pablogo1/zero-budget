b1b8ce2705c9c339bdd46076d1fc4b7b798d50cc
Pablo Gonzalez <3784129+pablogo1@users.noreply.github.com>
2025-04-30 23:39:51 -0600

    u Budgets/Create added month and year.

--- src/ZeroBudget/Features/Budgets/Create.cshtml.cs
+++ src/ZeroBudget/Features/Budgets/Create.cshtml.cs
@@ -10,6 +10,8 @@ public class CreateModel : PageModel
     public BudgetDefinition BudgetDefinition { get; set; } = default!;
     public string? ErrorMessage { get; set; } = null;
 
+    public Month[] Months { get; } = Month.Months;
+
     public void OnGet()
     {
         BudgetDefinition = new BudgetDefinition();
@@ -42,8 +44,38 @@ public sealed class BudgetDefinition
     [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Name can only contain letters, numbers, and spaces.")]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
+    [Display(Name = "Month")]
+    [DataType(DataType.Text)]
+    public int Month { get; set; } = DateTime.Today.Month + 1;
+
+    [Required]
+    [Display(Name = "Year")]
+    [DataType(DataType.Text)]
+    public int Year { get; set; } = DateTime.Today.Year;
+
+
     public BudgetDefinition()
     {
         
     }
+}
+
+public record Month(int Number, string Name)
+{
+    public static Month[] Months =>
+    [
+        new Month(1, "January"),
+        new Month(2, "February"),
+        new Month(3, "March"),
+        new Month(4, "April"),
+        new Month(5, "May"),
+        new Month(6, "June"),
+        new Month(7, "July"),
+        new Month(8, "August"),
+        new Month(9, "September"),
+        new Month(10, "October"),
+        new Month(11, "November"),
+        new Month(12, "December")
+    ];
 }
